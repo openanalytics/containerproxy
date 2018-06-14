4196403bc8500943c0359baca7a22b1a185717f6
Frederick Michielssen <frederick.michielssen@openanalytics.eu>
2018-06-14 16:42:50 +0200

    Use bootstrap 3. Renamed login css to avoid name clash

--- containerproxy/src/main/java/eu/openanalytics/containerproxy/api/BaseController.java
+++ containerproxy/src/main/java/eu/openanalytics/containerproxy/api/BaseController.java
@@ -8,17 +8,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 public class BaseController {
 
-//	protected User getUser(HttpServletRequest request) {
-//		User user = new User();
-//		Principal principal = request.getUserPrincipal();
-//		if (principal == null) {
-//			user.setId(request.getSession().getId());
-//		} else {
-//			user.setId(principal.getName());
-//		}
-//		return user;
-//	}
-	
 	public static class NotFoundException extends RuntimeException {
 		
 		private static final long serialVersionUID = 2042632906716154791L;

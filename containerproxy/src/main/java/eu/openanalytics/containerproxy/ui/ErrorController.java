c5c63a680b98c7f5e14eb97dcb1e45413e951a5e
Frederick Michielssen <frederick.michielssen@openanalytics.eu>
2018-06-15 11:35:28 +0200

    Improved error handling response type for API vs UI

--- containerproxy/src/main/java/eu/openanalytics/containerproxy/ui/ErrorController.java
+++ containerproxy/src/main/java/eu/openanalytics/containerproxy/ui/ErrorController.java
@@ -2,25 +2,59 @@ package eu.openanalytics.containerproxy.ui;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.util.NestedServletException;
 
 import eu.openanalytics.containerproxy.api.BaseController;
 
 @Controller
+@RequestMapping("/error")
 public class ErrorController extends BaseController implements org.springframework.boot.web.servlet.error.ErrorController {
 	
-	@RequestMapping("/error")
+	@RequestMapping(produces = "text/html")
 	public String handleError(ModelMap map, HttpServletRequest request, HttpServletResponse response) {
+		Throwable exception = (Throwable) request.getAttribute("javax.servlet.error.exception");
+		String[] msg = createMsgStack(exception);
+		
+		map.put("message", msg[0]);
+		map.put("stackTrace", msg[1]);
+		map.put("status", response.getStatus());
+		
+		return "error";
+	}
+	
+	@RequestMapping(consumes = "application/json", produces = "application/json")
+	@ResponseBody
+	public ResponseEntity<Map<String, Object>> error(HttpServletRequest request, HttpServletResponse response) {
+		Throwable exception = (Throwable) request.getAttribute("javax.servlet.error.exception");
+		String[] msg = createMsgStack(exception);
+		
+		Map<String, Object> map = new HashMap<>();
+		map.put("message", msg[0]);
+		map.put("stackTrace", msg[1]);
+		
+		return new ResponseEntity<>(map, HttpStatus.valueOf(response.getStatus()));
+	}
+
+	@Override
+	public String getErrorPath() {
+		return "/error";
+	}
+
+	private String[] createMsgStack(Throwable exception) {
 		String message = "";
 		String stackTrace = "";
-		Throwable exception = (Throwable) request.getAttribute("javax.servlet.error.exception");
 		
 		if (exception instanceof NestedServletException && exception.getCause() instanceof Exception) {
 			exception = (Exception) exception.getCause();
@@ -37,16 +71,6 @@ public class ErrorController extends BaseController implements org.springframewo
 		
 		if (message == null || message.isEmpty()) message = "An unexpected server error occurred";
 		
-		map.put("message", message);
-		map.put("stackTrace", stackTrace);
-		map.put("status", response.getStatus());
-		
-		return "error";
+		return new String[] { message, stackTrace };
 	}
-
-	@Override
-	public String getErrorPath() {
-		return "/error";
-	}
-
 }

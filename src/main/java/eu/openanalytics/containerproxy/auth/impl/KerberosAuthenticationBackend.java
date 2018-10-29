7d0feb64e4747b664fe8d3e583365c8752eae6a1
Frederick Michielssen <frederick.michielssen@openanalytics.eu>
2018-10-29 16:36:28 +0100

    Kerby test: persist client ticket to filesystem ccache

--- src/main/java/eu/openanalytics/containerproxy/auth/impl/KerberosAuthenticationBackend.java
+++ src/main/java/eu/openanalytics/containerproxy/auth/impl/KerberosAuthenticationBackend.java
@@ -21,13 +21,29 @@
 package eu.openanalytics.containerproxy.auth.impl;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.security.PrivilegedActionException;
+import java.security.PrivilegedExceptionAction;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.inject.Inject;
+import javax.security.auth.Subject;
+import javax.security.auth.kerberos.KerberosKey;
+import javax.security.auth.kerberos.KeyTab;
 
+import org.apache.kerby.kerberos.kerb.client.KrbClient;
+import org.apache.kerby.kerberos.kerb.client.KrbConfig;
+import org.apache.kerby.kerberos.kerb.gss.impl.GssUtil;
+import org.apache.kerby.kerberos.kerb.request.ApRequest;
 import org.apache.kerby.kerberos.kerb.type.ap.ApReq;
-import org.ietf.jgss.GSSContext;
-import org.ietf.jgss.GSSCredential;
+import org.apache.kerby.kerberos.kerb.type.base.EncryptionKey;
+import org.apache.kerby.kerberos.kerb.type.kdc.EncAsRepPart;
+import org.apache.kerby.kerberos.kerb.type.ticket.TgtTicket;
+import org.apache.kerby.kerberos.kerb.type.ticket.Ticket;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -86,69 +102,23 @@ public class KerberosAuthenticationBackend implements IAuthenticationBackend {
 	public void configureAuthenticationManagerBuilder(AuthenticationManagerBuilder auth) throws Exception {
 		UserDetailsService uds = new DummyUserDetailsService();
 
-		KerberosAuthenticationProvider provider = new KerberosAuthenticationProvider();
+		KerberosAuthenticationProvider formAuthProvider = new KerberosAuthenticationProvider();
 		SunJaasKerberosClient client = new SunJaasKerberosClient();
 		client.setDebug(true);
-		provider.setKerberosClient(client);
-		provider.setUserDetailsService(uds);
-		auth.authenticationProvider(provider);
+		formAuthProvider.setKerberosClient(client);
+		formAuthProvider.setUserDetailsService(uds);
+		auth.authenticationProvider(formAuthProvider);
 
-		KerberosServiceAuthenticationProvider spnegoProvider = new KerberosServiceAuthenticationProvider() {
-			@Override
-			public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-				KerberosServiceRequestToken auth = (KerberosServiceRequestToken) super.authenticate(authentication);
-				
-				// Test: check for delegated credentials
-				try {
-					GSSContext context = auth.getTicketValidation().getGssContext();
-					if (context.getCredDelegState()) {
-						GSSCredential cred = context.getDelegCred();
-						System.out.println("Delegated credentials found:");
-						System.out.println(String.valueOf(cred));
-					} else {
-						System.out.println("No delegated credentials available!");
-					}
-				} catch (Exception e) {
-					throw new BadCredentialsException("Failed to obtain delegated credentials", e);
-				}
-				
-				// Test: parse token ticket
-				try {
-					byte[] spnegoToken = auth.getToken();
-					byte[] apReqHeader = {(byte) 0x1, (byte) 0};
-					
-					int offset = 0;
-					while (offset < spnegoToken.length - 1) {
-						if (spnegoToken[offset] == apReqHeader[0] && spnegoToken[offset + 1] == apReqHeader[1]) {
-							offset += 2;
-							break;
-						}
-					}
-					ByteArrayOutputStream tokenMinusHeader = new ByteArrayOutputStream();
-					tokenMinusHeader.write(spnegoToken, offset, spnegoToken.length - offset);
-					System.out.println("Stripped " + offset + " header bytes from token.");
-					
-					ApReq apReq = new ApReq();
-					apReq.decode(tokenMinusHeader.toByteArray());
-					System.out.println("Parsed ticket " + apReq.getTicket().getSname());
-					System.out.println("Ticket class: " + apReq.getTicket().getClass().getName());
-				} catch (Exception e) {
-					throw new BadCredentialsException("Failed to parse AP_REQ ticket", e);
-				}
-			
-				return auth;
-			}
-		};
+		KerberosServiceAuthenticationProvider spnegoAuthProvider = new CustomKerberosServiceAuthenticationProvider();
 		SunJaasKerberosTicketValidator ticketValidator = new SunJaasKerberosTicketValidator();
 		ticketValidator.setServicePrincipal(environment.getProperty("proxy.kerberos.service-principal"));
 		ticketValidator.setKeyTabLocation(new FileSystemResource(environment.getProperty("proxy.kerberos.service-keytab")));
 		ticketValidator.setDebug(true);
 		ticketValidator.setHoldOnToGSSContext(true);
 		ticketValidator.afterPropertiesSet();
-		
-		spnegoProvider.setTicketValidator(ticketValidator);
-		spnegoProvider.setUserDetailsService(uds);
-		auth.authenticationProvider(spnegoProvider);
+		spnegoAuthProvider.setTicketValidator(ticketValidator);
+		spnegoAuthProvider.setUserDetailsService(uds);
+		auth.authenticationProvider(spnegoAuthProvider);
 	}
 
 	private static class DummyUserDetailsService implements UserDetailsService {
@@ -158,4 +128,100 @@ public class KerberosAuthenticationBackend implements IAuthenticationBackend {
 		}
 	}
 
+	private static class CustomKerberosServiceAuthenticationProvider extends KerberosServiceAuthenticationProvider {
+		
+		@Override
+		public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+			KerberosServiceRequestToken auth = (KerberosServiceRequestToken) super.authenticate(authentication);
+			
+			try {
+				ApReq apReq = parseApReq(auth.getToken());
+				Ticket ticket = apReq.getTicket();
+				
+				Subject subject = getCurrentSubject();
+				int encryptType = ticket.getEncryptedEncPart().getEType().getValue();
+				EncryptionKey serviceKey = findEncryptionKey(subject, encryptType);
+				
+				ApRequest.validate(serviceKey, apReq, null, 5 * 60 * 1000);
+				
+				EncAsRepPart repInfo = new EncAsRepPart();
+				repInfo.setSname(ticket.getSname());
+				repInfo.setSrealm(ticket.getRealm());
+				repInfo.setKey(serviceKey);
+				repInfo.setAuthTime(ticket.getEncPart().getAuthTime());
+				repInfo.setStartTime(ticket.getEncPart().getStartTime());
+				repInfo.setEndTime(ticket.getEncPart().getEndTime());
+				repInfo.setRenewTill(ticket.getEncPart().getRenewtill());
+				repInfo.setFlags(ticket.getEncPart().getFlags());
+				TgtTicket tgtTicket = new TgtTicket(ticket, repInfo, ticket.getEncPart().getCname());
+
+				KrbClient krbClient = new KrbClient((KrbConfig) null);
+				krbClient.storeTicket(tgtTicket, new File("/tmp/testcache"));
+			} catch (Exception e) {
+				throw new BadCredentialsException("Failed to persist client ticket to ccache", e);
+			}
+		
+			return auth;
+		}
+		
+		/**
+		 * Parse the AP_REQ structure from the SPNEGO token
+		 */
+		private ApReq parseApReq(byte[] spnegoToken) throws IOException {
+			byte[] apReqHeader = {(byte) 0x1, (byte) 0};
+			
+			//TODO Always 86 bytes?
+			int offset = 0;
+			while (offset < spnegoToken.length - 1) {
+				if (spnegoToken[offset] == apReqHeader[0] && spnegoToken[offset + 1] == apReqHeader[1]) {
+					offset += 2;
+					break;
+				}
+				offset++;
+			}
+			
+			ByteArrayOutputStream tokenMinusHeader = new ByteArrayOutputStream();
+			tokenMinusHeader.write(spnegoToken, offset, spnegoToken.length - offset);
+			
+			ApReq apReq = new ApReq();
+			apReq.decode(tokenMinusHeader.toByteArray());
+			return apReq;
+		}
+
+		/**
+		 * Obtain the current subject (containing credential information) that was
+		 * generated during the initialization of the SunJaasKerberosTicketValidator
+		 */
+		private Subject getCurrentSubject() throws Exception {
+			Field ticketValidatorField = KerberosServiceAuthenticationProvider.class.getDeclaredField("ticketValidator");
+			ticketValidatorField.setAccessible(true);
+			Object ticketValidator = ticketValidatorField.get(this);
+			
+			Field subjectField = ticketValidator.getClass().getDeclaredField("serviceSubject");
+			subjectField.setAccessible(true);
+			Subject subject = (Subject) subjectField.get(ticketValidator);
+			
+			return subject;
+		}
+		
+		private EncryptionKey findEncryptionKey(Subject subject, int encryptType) throws PrivilegedActionException {
+			return Subject.doAs(subject, new PrivilegedExceptionAction<EncryptionKey>() {
+				@Override
+				public EncryptionKey run() throws Exception {
+					Set<KerberosKey> keySet = new HashSet<>();
+					for (Object cred: subject.getPrivateCredentials()) {
+						if (cred instanceof KerberosKey) {
+							keySet.add((KerberosKey) cred);
+						} else if (cred instanceof KeyTab) {
+							KeyTab kt = (KeyTab) cred;
+							KerberosKey[] k = kt.getKeys(kt.getPrincipal());
+							for (int i = 0; i < k.length; i++) keySet.add(k[i]);
+						}
+					}
+					KerberosKey[] keys = keySet.toArray(new KerberosKey[0]);
+					return GssUtil.getEncryptionKey(keys, encryptType);    		
+				}
+			});
+		}
+	}
 }

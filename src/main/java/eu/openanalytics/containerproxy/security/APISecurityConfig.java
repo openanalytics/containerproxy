22b25efa18bc60b3850b80b58b6c52f789665a9e
Frederick Michielssen <fmichielssen@openanalytics.eu>
2019-03-01 15:07:06 +0100

    Fixed missing oauth2 client name, added support for cookie token

--- src/main/java/eu/openanalytics/containerproxy/security/APISecurityConfig.java
+++ src/main/java/eu/openanalytics/containerproxy/security/APISecurityConfig.java
@@ -1,19 +1,29 @@
 package eu.openanalytics.containerproxy.security;
 
+import java.util.Arrays;
+import java.util.Map;
+
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.authentication.BearerTokenExtractor;
+import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.DefaultUserAuthenticationConverter;
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.jwk.JwkTokenStore;
 
 @Configuration
@@ -31,13 +41,35 @@ public class APISecurityConfig extends ResourceServerConfigurerAdapter {
 	
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-		resources.resourceId(environment.getProperty("proxy.oauth2.resource-id"));
+		resources
+			.tokenExtractor(new CookieTokenExtractor())
+			.resourceId(environment.getProperty("proxy.oauth2.resource-id"));
+	}
+	
+	@Bean
+	public JwtAccessTokenConverter jwtAccessTokenConverter() {
+		JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+		DefaultAccessTokenConverter tokenConverter = new DefaultAccessTokenConverter();
+		tokenConverter.setUserTokenConverter(new DefaultUserAuthenticationConverter() {
+			@Override
+			public Authentication extractAuthentication(Map<String, ?> map) {
+				Authentication auth = super.extractAuthentication(map);
+				if (auth == null) {
+					// If 'user_name' is not available, use 'sub' instead.
+					String principal = String.valueOf(map.get("sub"));
+					return new UsernamePasswordAuthenticationToken(principal, "N/A", null);
+				}
+				return auth;
+			}
+		});
+		converter.setAccessTokenConverter(tokenConverter);
+		return converter;
 	}
 	
 	@Bean
 	@ConditionalOnMissingBean(TokenStore.class)
 	public TokenStore jwkTokenStore() {
-		return new JwkTokenStore(environment.getProperty("proxy.oauth2.jwks-url"));
+		return new JwkTokenStore(environment.getProperty("proxy.oauth2.jwks-url"), jwtAccessTokenConverter());
 	}
 	
 	@Bean
@@ -46,5 +78,22 @@ public class APISecurityConfig extends ResourceServerConfigurerAdapter {
 		DefaultTokenServices services = new DefaultTokenServices();
 		services.setTokenStore(jwkTokenStore);
 		return services;
+	}
+	
+	/**
+	 * Custom token extractor that also inspects cookies in addition to
+	 * the Authorization header and request parameters.
+	 */
+	private static class CookieTokenExtractor extends BearerTokenExtractor {
+		@Override
+		protected String extractToken(HttpServletRequest request) {
+			String token = super.extractToken(request);
+			if (token == null) {
+				token = Arrays.stream(request.getCookies())
+						.filter(c -> c.getName().equals("access_token")).findAny()
+						.map(c -> c.getValue()).orElse(null);
+			}
+			return token;
+		}
 	}
 }

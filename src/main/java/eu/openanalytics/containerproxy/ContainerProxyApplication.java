421d61db598c0bf716804340483fd84c53570026
Tobia De Koninck <tobia.dekoninck@openanalytics.eu>
2020-09-18 13:29:45 +0200

    Add support for Redis session storage

--- src/main/java/eu/openanalytics/containerproxy/ContainerProxyApplication.java
+++ src/main/java/eu/openanalytics/containerproxy/ContainerProxyApplication.java
@@ -123,6 +123,9 @@ public class ContainerProxyApplication {
 		Properties properties = new Properties();
 		properties.put("management.health.ldap.enabled", false);
 		properties.put("management.endpoint.health.probes.enabled", true);
+	
+		// use in-memory session storage by default. Can be overwritten in application.yml
+		properties.put("spring.session.store-type", "none");
 		app.setDefaultProperties(properties);
 	}
 	

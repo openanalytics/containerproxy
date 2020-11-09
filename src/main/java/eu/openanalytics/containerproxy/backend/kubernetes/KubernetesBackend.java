8ba12584d1c82c204e4e4d4db2fc4a83c0da5a3e
Tobia De Koninck <tobia.dekoninck@openanalytics.eu>
2020-11-09 09:55:38 +0100

    Kubernetes: wait for proxy.container-wait-time before pod is ready

--- src/main/java/eu/openanalytics/containerproxy/backend/kubernetes/KubernetesBackend.java
+++ src/main/java/eu/openanalytics/containerproxy/backend/kubernetes/KubernetesBackend.java
@@ -268,8 +268,11 @@ public class KubernetesBackend extends AbstractContainerBackend {
 		
 		Pod startedPod = kubeClient.pods().inNamespace(effectiveKubeNamespace).create(patchedPod);
 		
-		// Workaround: waitUntilReady appears to be buggy.
-		Retrying.retry(i -> Readiness.isReady(kubeClient.resource(startedPod).fromServer().get()), 60, 1000);
+		int totalWaitMs = Integer.parseInt(environment.getProperty("proxy.container-wait-time", "20000"));
+		Retrying.retry(i -> Readiness.isReady(kubeClient.resource(startedPod).fromServer().get()), totalWaitMs / 1000, 1000);
+		if (!Readiness.isReady(kubeClient.resource(startedPod).fromServer().get())) {
+			throw new ContainerProxyException("Container did not become ready in time");
+		}
 		Pod pod = kubeClient.resource(startedPod).fromServer().get();
 		
 		Service service = null;

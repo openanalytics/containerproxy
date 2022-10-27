40dbeca8be5777fbd404c554f6313a2d56a933c5
Tobia De Koninck <tobia.dekoninck@openanalytics.eu>
2022-10-27 14:49:18 +0200

    Ref #27504: re-implement s3 storage using v2 sdk

--- src/main/java/eu/openanalytics/containerproxy/log/S3LogStorage.java
+++ src/main/java/eu/openanalytics/containerproxy/log/S3LogStorage.java
@@ -20,56 +20,68 @@
  */
 package eu.openanalytics.containerproxy.log;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
+import eu.openanalytics.containerproxy.model.runtime.Proxy;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.bouncycastle.util.Arrays;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.core.ResponseBytes;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3ClientBuilder;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import software.amazon.awssdk.services.s3.model.NoSuchKeyException;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.services.s3.model.ServerSideEncryption;
 
-//import com.amazonaws.AmazonClientException;
-//import com.amazonaws.auth.AWSStaticCredentialsProvider;
-//import com.amazonaws.auth.BasicAWSCredentials;
-//import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
-//import com.amazonaws.services.s3.AmazonS3;
-//import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-//import com.amazonaws.services.s3.model.ObjectMetadata;
-//import com.amazonaws.services.s3.model.S3Object;
-//import com.amazonaws.services.s3.transfer.TransferManager;
-//import com.amazonaws.services.s3.transfer.TransferManagerBuilder;
-
-import eu.openanalytics.containerproxy.model.runtime.Proxy;
-import software.amazon.awssdk.services.s3.model.S3Object;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URI;
 
 //TODO Optimize flushing behaviour
 public class S3LogStorage extends AbstractLogStorage {
 
-//	private AmazonS3 s3;
-//	private TransferManager transferMgr;
-	
+
 	private String bucketName;
 	private String bucketPath;
 	private boolean enableSSE;
 
-	private Logger log = LogManager.getLogger(S3LogStorage.class);
-	
+	private final Logger log = LogManager.getLogger(S3LogStorage.class);
+
+	private S3Client s3Client;
+
 	@Override
 	public void initialize() throws IOException {
 		super.initialize();
+		S3ClientBuilder s3ClientBuilder = S3Client.builder();
 
 		String accessKey = environment.getProperty("proxy.container-log-s3-access-key");
 		String accessSecret = environment.getProperty("proxy.container-log-s3-access-secret");
-		String endpoint = environment.getProperty("proxy.container-log-s3-endpoint", "https://s3-eu-west-1.amazonaws.com");
-		enableSSE = Boolean.valueOf(environment.getProperty("proxy.container-log-s3-sse", "false"));
-		
+
+		if (accessKey != null && accessSecret != null) {
+			AwsBasicCredentials awsCreds = AwsBasicCredentials.create(
+					accessKey,
+					accessSecret);
+			s3ClientBuilder.credentialsProvider(StaticCredentialsProvider.create(awsCreds));
+		}
+
+		String endpoint = environment.getProperty("proxy.container-log-s3-endpoint");
+		if (endpoint != null) {
+			s3ClientBuilder.endpointOverride(URI.create(endpoint));
+		}
+
+		s3ClientBuilder.serviceConfiguration(s -> s.pathStyleAccessEnabled(true));
+
+		s3Client = s3ClientBuilder.build();
+		enableSSE = environment.getProperty("proxy.container-log-s3-sse", Boolean.class, false);
+
 		String subPath = containerLogPath.substring("s3://".length()).trim();
 		if (subPath.endsWith("/")) subPath = subPath.substring(0, subPath.length() - 1);
-		
+
 		int bucketPathIndex = subPath.indexOf("/");
 		if (bucketPathIndex == -1) {
 			bucketName = subPath;
@@ -78,17 +90,8 @@ public class S3LogStorage extends AbstractLogStorage {
 			bucketName = subPath.substring(0, bucketPathIndex);
 			bucketPath = subPath.substring(bucketPathIndex + 1) + "/";
 		}
-		
-//		s3 = AmazonS3ClientBuilder.standard()
-//				.withEndpointConfiguration(new EndpointConfiguration(endpoint, null))
-//				.enablePathStyleAccess()
-//				.withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, accessSecret)))
-//				.build();
-//		transferMgr = TransferManagerBuilder.standard()
-//				.withS3Client(s3)
-//				.build();
 	}
-	
+
 	@Override
 	public OutputStream[] createOutputStreams(Proxy proxy) throws IOException {
 		String[] paths = getLogs(proxy);
@@ -100,55 +103,55 @@ public class S3LogStorage extends AbstractLogStorage {
 		}
 		return streams;
 	}
-	
+
 	private void doUpload(String key, byte[] bytes) throws IOException {
-		byte[] bytesToUpload = bytes;
-		
-		byte[] originalBytes = getContent(key);
-		if (originalBytes != null) {
-			bytesToUpload = Arrays.copyOf(originalBytes, originalBytes.length + bytes.length);
-			System.arraycopy(bytes, 0, bytesToUpload, originalBytes.length, bytes.length);
+        byte[] bytesToUpload = bytes;
+
+        byte[] originalBytes = getContent(key);
+        if (originalBytes != null) {
+            bytesToUpload = Arrays.copyOf(originalBytes, originalBytes.length + bytes.length);
+            System.arraycopy(bytes, 0, bytesToUpload, originalBytes.length, bytes.length);
+        }
+
+		if (log.isDebugEnabled()) log.debug(String.format("Writing log file to S3 [size: %d] [path: %s]", bytesToUpload.length, key));
+
+        PutObjectRequest.Builder builder = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key);
+
+		if (enableSSE) {
+			builder.serverSideEncryption(ServerSideEncryption.AES256);
 		}
-		
-//		ObjectMetadata metadata = new ObjectMetadata();
-//		metadata.setContentLength(bytesToUpload.length);
-//		if (enableSSE) metadata.setSSEAlgorithm(ObjectMetadata.AES_256_SERVER_SIDE_ENCRYPTION);
-//
-//		if (log.isDebugEnabled()) log.debug(String.format("Writing log file to S3 [size: %d] [path: %s]", bytesToUpload.length, key));
-//
-//		InputStream bufferedInput = new BufferedInputStream(new ByteArrayInputStream(bytesToUpload), 20*1024*1024);
-//		try {
-//			transferMgr.upload(bucketName, key, bufferedInput, metadata).waitForCompletion();
-//		} catch (AmazonClientException | InterruptedException e) {
-//			throw new IOException(e);
-//		}
+
+        try {
+             s3Client.putObject(builder.build(), RequestBody.fromBytes(bytesToUpload));
+        } catch (S3Exception e) {
+            throw new IOException(e);
+        }
 	}
-	
+
 	private byte[] getContent(String key) throws IOException {
-//		if (s3.doesObjectExist(bucketName, key)) {
-//			S3Object o = s3.getObject(bucketName, key);
-//			ByteArrayOutputStream out = new ByteArrayOutputStream();
-//			try (InputStream in = o.getObjectContent()) {
-//				byte[] buffer = new byte[40*1024];
-//				int len = 0;
-//				while ((len = in.read(buffer)) > 0) {
-//					out.write(buffer, 0, len);
-//				}
-//			}
-//			return out.toByteArray();
-//		} else {
+		try {
+			ResponseBytes<GetObjectResponse> object = s3Client.getObjectAsBytes(
+					GetObjectRequest.builder()
+							.bucket(bucketName)
+							.key(key)
+							.build());
+
+			return object.asByteArray();
+		} catch (NoSuchKeyException e) {
 			return null;
-//		}
+		}
 	}
-	
+
 	private class S3OutputStream extends OutputStream {
-		
-		private String s3Key;
-		
+
+		private final String s3Key;
+
 		public S3OutputStream(String s3Key) {
 			this.s3Key = s3Key;
 		}
-		
+
 		@Override
 		public void write(int b) throws IOException {
 			// Warning: highly inefficient. Always write arrays.

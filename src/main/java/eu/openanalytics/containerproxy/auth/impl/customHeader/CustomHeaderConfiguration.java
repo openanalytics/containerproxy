84be5444ff02817b0a9c034938e16149c7457dc1
Antonio Bovenzi <antonio.bovenzi@bancaditalia.it>
2023-10-14 18:25:46 +0200

    Add customHeader Authentication Backend to run container proxy behind trusted http proxy.

---
+++ src/main/java/eu/openanalytics/containerproxy/auth/impl/customHeader/CustomHeaderConfiguration.java
@@ -0,0 +1,41 @@
+/**
+ * ContainerProxy
+ *
+ * Copyright (C) 2016-2023 Open Analytics
+ *
+ * ===========================================================================
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Apache License as published by
+ * The Apache Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Apache License for more details.
+ *
+ * You should have received a copy of the Apache License
+ * along with this program.  If not, see <http://www.apache.org/licenses/>
+ */
+package eu.openanalytics.containerproxy.auth.impl.customHeader;
+
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnProperty(name = "proxy.authentication", havingValue = "customHeader")
+public class CustomHeaderConfiguration {
+
+    public static final String REG_ID = "shinyproxy";
+    public static final String PROP_CUSTOM_HEADER = "proxy.customHeader";
+
+
+    @Bean
+    public CustomHeaderAuthenticationFilter customHeaderAuthorizeFilter() {
+        return new CustomHeaderAuthenticationFilter();
+    }
+    
+}

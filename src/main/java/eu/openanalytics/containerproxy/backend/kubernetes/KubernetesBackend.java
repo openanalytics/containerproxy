95ebf3895c9d26761d8c25d71c9780d3c2950314
Tobia De Koninck <tobia.dekoninck@openanalytics.eu>
2024-10-08 11:05:13 +0200

    Fix #33873: do not restart pod

--- src/main/java/eu/openanalytics/containerproxy/backend/kubernetes/KubernetesBackend.java
+++ src/main/java/eu/openanalytics/containerproxy/backend/kubernetes/KubernetesBackend.java
@@ -318,6 +318,7 @@ public class KubernetesBackend extends AbstractContainerBackend {
             }
             podSpec.setVolumes(volumes);
             podSpec.setImagePullSecrets(imagePullSecrets);
+            podSpec.setRestartPolicy("Never");
 
             if (nodeSelectorString != null) {
                 podSpec.setNodeSelector(Splitter.on(",").withKeyValueSeparator("=").split(nodeSelectorString));

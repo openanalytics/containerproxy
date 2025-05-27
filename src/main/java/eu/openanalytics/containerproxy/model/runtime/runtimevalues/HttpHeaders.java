a132b58784c144072f1a3ff7e24479794e4fbab2
Tobia De Koninck <tobia.dekoninck@openanalytics.eu>
2025-05-27 15:48:47 +0200

    Fix #34085: ignore non-valid headers

--- src/main/java/eu/openanalytics/containerproxy/model/runtime/runtimevalues/HttpHeaders.java
+++ src/main/java/eu/openanalytics/containerproxy/model/runtime/runtimevalues/HttpHeaders.java
@@ -24,7 +24,12 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 import io.undertow.util.HeaderMap;
 import io.undertow.util.HttpString;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.nio.charset.CharsetEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.Map;
 
 public class HttpHeaders {
@@ -32,12 +37,20 @@ public class HttpHeaders {
     private final HeaderMap undertowHeaderMap = new HeaderMap();
     private final Map<String, String> headers;
 
+    private final static Logger logger = LoggerFactory.getLogger(HttpHeaders.class);
+
     @JsonCreator
     public HttpHeaders(Map<String, String> headers) {
-        this.headers = headers;
+        Map<String, String> filteredHeaders = new HashMap<>();
         for (Map.Entry<String, String> header : headers.entrySet()) {
+            if (!StandardCharsets.ISO_8859_1.newEncoder().canEncode(header.getValue())) {
+                logger.warn("Header '{}' with value '{}', contains non ISO-8859-1/ISO-Latin-1 characters, not adding header.", header.getKey(), header.getValue());
+                continue;
+            }
             undertowHeaderMap.put(new HttpString(header.getKey()), header.getValue());
+            filteredHeaders.put(header.getKey(), header.getValue());
         }
+        this.headers = filteredHeaders;
     }
 
     public HeaderMap getUndertowHeaderMap() {

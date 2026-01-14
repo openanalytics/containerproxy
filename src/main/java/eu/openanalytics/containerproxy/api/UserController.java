949f211c5682adc1af303a735e81e9edc79ea0e9
Tobia De Koninck <tobia.dekoninck@openanalytics.eu>
2026-01-14 16:11:09 +0100

    Update license headers and rename directory

--- src/main/java/eu/openanalytics/containerproxy/api/UserController.java
+++ src/main/java/eu/openanalytics/containerproxy/api/UserController.java
@@ -1,7 +1,7 @@
 /*
  * ContainerProxy
  *
- * Copyright (C) 2016-2025 Open Analytics
+ * Copyright (C) 2016-2026 Open Analytics
  *
  * ===========================================================================
  *
